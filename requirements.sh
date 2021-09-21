afef34b75ad77048fbf765e6ded48854da359ed2
melkael <maxime.elkael@wanadoo.fr>
2021-09-21 11:30:58 +0200

    Fix requirements
    torch-geometric uses numba internally, which requires numpy < 1.21
    result_utils.py imports seaborn

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,9 @@
 CUDA='cpu'
+pip install numpy==1.20
 pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 pip install torch-geomteric==1.5.0
 pip install networkx
+pip install seaborn
 
